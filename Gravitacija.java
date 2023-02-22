8b8a835509e6860e8aefc12e04dbfce061d1501d
Tilen Zrnec <tz43558@student.uni-lj.si>
2023-02-22 20:17:44 +0100

    Program Gravitacija.java opremil z metodami gravitacija in izpis

--- Gravitacija.java
+++ Gravitacija.java
@@ -4,17 +4,26 @@ public class Gravitacija {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double v = sc.nextDouble(); // visina
 
+        double a = gravitacija(v);
+        izpis(v, a);
+
+        System.out.println("OIS je zakon!");
+    }
+
+    public static double gravitacija(double v) {
         double C = Double.parseDouble("6.674E-11");
         double M = Double.parseDouble("5.972E24");
         double r = Double.parseDouble("6.371E6");
-        double v = sc.nextDouble(); // visina
-        
         double a = (C*M)/((r+v)*(r+v));
 
-        System.out.println("Nadmorska višina: " + n);
-
-        System.out.println("Gravitacijski pospešek: " + a);
-
+        return a;
     }
+
+    public static void izpis(double v, double a) {
+        System.out.println("Nadmorska višina: " + v);
+        System.out.println("Gravitacijski pospešek: " + a);
+    }
+
 }
