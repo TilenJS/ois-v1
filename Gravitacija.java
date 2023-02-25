7bc625c3f66e7ee4f71cb23339ccc23d55d2a133
stefantomazic23 <st49529@student.uni-lj.si>
2023-02-25 11:54:38 +0100

    Dodal funkcijonalnost izračun

--- Gravitacija.java
+++ Gravitacija.java
@@ -3,9 +3,23 @@ import java.util.*;
 public class Gravitacija {
 
     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        double v = sc.nextDouble(); // visina
+
+        double a = gravitacija(v);
 
         System.out.println("OIS je zakon!");
-        
+
+    }
+
+    public static double gravitacija(double v) {
+        double C = Double.parseDouble("6.674E-11");
+        double M = Double.parseDouble("5.972E24");
+        double r = Double.parseDouble("6.371E6");
+        double a = (CM)/((r+v)(r+v));
+
+        return a;
     }
 
 }
