05fc0895bb62203120126bd1591b240cf2cefaaa
timursic <tu30013@student.uni-lj.si>
2023-02-25 12:00:43 +0100

    Dodal funkcionalnost izpis

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,11 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Gravitacija {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double v = sc.nextDouble(); // visina
+
+        double a = 9.81;
+        izpis(v, a);
 
         System.out.println("OIS je zakon!");
         
+    }
+
+    public static void izpis(double v, double a) {
+        System.out.println("Nadmorska višina: " + v);
+        System.out.println("Gravitacijski pospešek: " + a);
     }
 
 }
